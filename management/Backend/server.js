7aebca6ccb1c1bd1a45a67c1bda6afba4699e6f4
YOUR_GITHUB_USERNAME <YOUR_EMAIL@example.com>
2026-03-14 05:09:20 +0530

    updated cors for vercel frontend

--- management/Backend/server.js
+++ management/Backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // CORS Configuration
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5173' ,  'https://bankmanagement4-6dqsysu85-jayendras-projects-60719684.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
